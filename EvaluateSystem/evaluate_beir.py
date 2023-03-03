b44afe2694d82dc33a766ee1212ce84e6bb1f9e8
Carlos Dominguez <cdominguezbecerril@gmail.com>
2023-03-03 13:51:35 +0100

    BEIR evaluation

---
+++ EvaluateSystem/evaluate_beir.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+
+def create_folders(args):
+    for folder_name in ["output_and_error_files", "results"]:
+        folder_path = os.path.join(args.beir_save_path, folder_name)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path, exist_ok=True)
+
+def create_slurm(args):
+    slurm_file = f"""#!/bin/tcsh
+#SBATCH --job-name=test_beir
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=10-00:00:00
+#SBATCH --mem=32GB
+#SBATCH --gres=gpu:1
+#SBATCH --output={args.beir_save_path}/output_and_error_files/output.txt
+#SBATCH --error={args.beir_save_path}/output_and_error_files/error.txt
+
+# Activamos el entorno virtual que necesitemos
+source ~/.tcshrc
+
+# LLamamos a nuestro script de python
+srun python3 /gscratch3/users/cdominguez019/PhD/InformationRetrieval/EvaluateSystem/evaluation_script.py \\
+        --beir_datasets_name {args.beir_datasets_name} \\
+        --beir_datasets_path {args.beir_datasets_path} \\
+        --beir_save_path {args.beir_save_path} \\
+        --similarity {args.similarity} \\
+        --model_path {args.model_path}""".replace(",", "").replace("[", "").replace("]", "")
+
+    slurm_path = f"{args.beir_save_path}/output_and_error_files/tmp.slurm"
+    with open(slurm_path, "w") as f:
+        f.write(slurm_file)
+
+    os.system(f"sbatch {slurm_path}")
+
+def evaluate_datasets(args):
+    if isinstance(args.beir_datasets_name, str):
+        args.beir_datasets_name = [args.beir_datasets_name]
+    
+    if args.beir_datasets_name == ["all"]:
+        args.beir_datasets_name = [
+            "scifact", "msmarco", "trec-covid", "nfcorpus", "nq", "hotpotqa", "fiqa", "arguana", 
+            "webis-touche2020", "cqadupstack", "quora", "dbpedia-entity", "scidocs", 
+            "fever", "climate-fever"
+    ]
+
+    create_folders(args)
+    create_slurm(args)
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--beir_datasets_name", nargs="+")
+    parser.add_argument("--beir_datasets_path", type=str)
+    parser.add_argument("--beir_save_path", type=str)
+    parser.add_argument("--model_path", type=str)
+    parser.add_argument("--similarity", type=str)
+    args, _ = parser.parse_known_args()
+
+    print("BEIR Evaluation")
+    print("BEIR datasets name:", args.beir_datasets_name)
+    print("BEIR datasets path:", args.beir_datasets_path)
+    print("BEIR save path:", args.beir_save_path)
+    print("BEIR model path:", args.model_path)
+
+    evaluate_datasets(args)
---
+++ EvaluateSystem/evaluation_script.py
@@ -0,0 +1,95 @@
+from beir import util, LoggingHandler
+from beir.retrieval import models
+from beir.datasets.data_loader import GenericDataLoader
+from beir.retrieval.evaluation import EvaluateRetrieval
+from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
+
+import logging
+import pathlib, os
+
+import argparse
+
+from collections import defaultdict
+
+import numpy as np
+
+import json
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+
+def evaluate_dataset(dataset_name, args):
+
+    data_path = os.path.join(args.beir_datasets_path, dataset_name)
+    split = "test" if dataset_name != "msmarco" else "dev"
+    retriever = args.retriever
+
+    metrics = defaultdict(list)  # store final results
+
+    if not dataset == "cqadupstack":
+        corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split=split)
+        results = retriever.retrieve(corpus, queries)
+        ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+        for metric in (ndcg, _map, recall, precision, "mrr", "recall_cap", "hole"):
+            if isinstance(metric, str):
+                metric = retriever.evaluate_custom(qrels, results, retriever.k_values, metric=metric)
+            for key, value in metric.items():
+                metrics[key].append(value)
+
+    elif dataset_name == "cqadupstack": # compute macroaverage over datasets
+        paths = glob.glob(data_path + "/*")
+        for path in paths:
+            corpus, queries, qrels = GenericDataLoader(data_folder=path).load(split=split)
+            results = retriever.retrieve(corpus, queries)
+            
+            ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+            for metric in (ndcg, _map, recall, precision, "mrr", "recall_cap", "hole"):
+                if isinstance(metric, str):
+                    metric = retriever.evaluate_custom(qrels, results, retriever.k_values, metric=metric)
+                for key, value in metric.items():
+                    metrics[key].append(value)
+
+        for key, value in metrics.items():
+            assert (
+                len(value) == 12
+            ), f"cqadupstack includes 12 datasets, only {len(value)} values were compute for the {key} metric"
+
+    else:
+        print(f"Dataset with name {dataset_name} doesn't exist")
+
+    metrics = {key: 100 * np.mean(value) for key, value in metrics.items()}
+
+    with open(f"{args.beir_save_path}/results/{dataset_name}_results.json", "w") as json_file:
+        json.dump(metrics, json_file)
+    
+    print(f"Dataset: {dataset}")
+    print(metrics)
+    print("NDCG@10", metrics["NDCG@10"])
+    print("Recall@100", metrics["Recall@100"])
+
+if __name__ == "__main__":
+
+    os.system("pwd")
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--beir_datasets_name", nargs="+")
+    parser.add_argument("--beir_datasets_path", type=str)
+    parser.add_argument("--beir_save_path", type=str)
+    parser.add_argument("--model_path", type=str)
+    parser.add_argument("--similarity", type=str)
+    args, _ = parser.parse_known_args()
+
+    model = DRES(models.SentenceBERT(args.model_path), batch_size=256, corpus_chunk_size=512*9999)
+    retriever = EvaluateRetrieval(model, score_function=args.similarity)
+    args.retriever = retriever
+
+    for dataset in args.beir_datasets_name:
+        evaluate_dataset(dataset, args)
+
+
+
+    
